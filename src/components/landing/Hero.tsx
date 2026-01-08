5ce7ede1c884f99470b766eb5be7f8b6dc737245
samandar6789kj-ui <samandar6789kj@gmail.com>
2026-01-08 11:36:14 +0500

    strelcani yuqatdim

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -56,7 +56,7 @@ export const Hero = component$(() => {
           class="h-6  w-6 text-[#4caf50]/30" 
           fill="none" 
           viewBox="0   0 24 24" 
-          stroke="currentColor"
+          
         >
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
         </svg>
